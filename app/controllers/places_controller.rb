6739e5852c56d9457f901c8ecc73ac981b0300f5
sarahwade11 <sarah.wade@kellogg.northwestern.edu>
2023-03-06 04:31:27 +0000

    HW4 v3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,30 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.where({ "user_id" => session["user_id"] })
+    @places = Place.new
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
+    @places = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"] })
   end
 
   def new
-    @place = Place.new
+    @places = Place.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+    if @current_user
+      @places = Place.new
+      @places["name"] = params["place"]["name"]
+      @places["user_id"] = @current_user["id"]
+      @places.save
+    end
+ 
     redirect_to "/places"
   end
 
-end
+end 
+
+
