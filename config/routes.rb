f9a953a1f75bfa8c5c91dc2be537d592f0a9baa0
sarahwade11 <sarah.wade@kellogg.northwestern.edu>
2023-03-06 03:49:02 +0000

    HW 4 v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
+  
   resources "places"
   resources "posts"
   resources "sessions"
